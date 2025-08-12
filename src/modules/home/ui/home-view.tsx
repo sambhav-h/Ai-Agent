840b4d36035bbc33f16ec1e2929f5ebb698c9ee9
sambhav-h <sambhavjain25102003@gmail.com>
2025-08-12 21:56:55 +0530

    feat: integrate TRPC for API handling, add query client setup, and implement greeting endpoint

--- src/modules/home/ui/home-view.tsx
+++ src/modules/home/ui/home-view.tsx
@@ -1,30 +1,144 @@
 "use client"
 
-import { Button } from "@/components/ui/button"
-import { authClient } from "@/lib/auth-client"
-import router from "next/router"
+import { useTRPC } from "@/trpc/client"
+import { useQuery } from "@tanstack/react-query"
 
 export const HomeView = () => {
-  const {data : session} = authClient.useSession()
+  const trpc = useTRPC()
+  const {data} = useQuery(trpc.hello.queryOptions({text: "Sambhav"}))
 
-  if(!session){
-    return(
-      <p>Loading...</p>
-    )
-  }
+  
   return (
     <div className="flex flex-col p-4 gap-y-4">
-      <p>Logged  in as {session.user.name}</p>
-      <Button onClick={() => authClient.signOut(
-        {
-            fetchOptions: {
-                onSuccess: async () => { 
-                    await router.push("/sign-in");
-                }
-            }
-        }) 
-    }>Sign Out</Button>
+      {data?.greeting}
     </div>
   )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Button } from "@/components/ui/button"
+// import { authClient } from "@/lib/auth-client"
+// import router from "next/router"
+
+// export const HomeView = () => {
+//   const {data : session} = authClient.useSession()
+
+//   if(!session){
+//     return(
+//       <p>Loading...</p>
+//     )
+//   }
+//   return (
+//     <div className="flex flex-col p-4 gap-y-4">
+//       <p>Logged  in as {session.user.name}</p>
+//       <Button onClick={() => authClient.signOut(
+//         {
+//             fetchOptions: {
+//                 onSuccess: async () => { 
+//                     await router.push("/sign-in");
+//                 }
+//             }
+//         }) 
+//     }>Sign Out</Button>
+//     </div>
+//   )
+// }
 
