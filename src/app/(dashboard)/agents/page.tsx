71c6043058f53d91d30f7496c48069c096cc614b
sambhav-h <sambhavjain25102003@gmail.com>
2025-08-21 23:41:00 +0530

    feat: implement agent management features including session handling, agent creation, and responsive UI components

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,13 +1,27 @@
+import { auth } from "@/lib/auth";
+import { AgentsListHeader } from "@/modules/agents/ui/components/agents-list-header";
 import { AgentsView, AgentsViewErrror, AgentsViewLoading } from "@/modules/agents/ui/views/agents-view";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
 import {ErrorBoundary} from "react-error-boundary"
-const Page = () => { 
+const Page = async () => {
+      const session = await auth.api.getSession({
+        headers: await headers()
+      })
+    
+      if(!session){
+        redirect("/sign-in")
+      }
+
     const queryClient = getQueryClient()
     void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions())
 
     return (
+        <>
+        <AgentsListHeader/>
         <HydrationBoundary state={dehydrate(queryClient)}>
             <Suspense fallback={<AgentsViewLoading/>}>
             <ErrorBoundary fallback={<AgentsViewErrror/>}>
@@ -15,6 +29,7 @@ const Page = () => {
             </ErrorBoundary>
             </Suspense>
         </HydrationBoundary>
+        </>
     )
     
 }
--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,13 +1,43 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { createTRPCRouter, baseProcedure } from "@/trpc/init";
+import { agentsInsertSchema } from "@/modules/schemas";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { TRPCError } from "@trpc/server";
+import { eq } from "drizzle-orm";
+import {z} from "zod"
 
 
 export const agentsRouter = createTRPCRouter({
-    getMany: baseProcedure.query(async() => {
+      getOne: protectedProcedure.input(z.object({id: z.string()})).query(async({input}) => {
+        const [existingAgent] = await db
+        .select()
+        .from(agents)
+        .where(eq(agents.id, input.id))
+        return existingAgent
+    }),
+    getMany: protectedProcedure.query(async() => {
         const data = await db
         .select()
         .from(agents);
         return data
-    })
+    }),
+
+create: protectedProcedure
+  .input(agentsInsertSchema)
+  .mutation(async ({ input, ctx }) => {
+    if (!ctx.auth?.user.id) {
+      throw new TRPCError({ code: "UNAUTHORIZED" });
+    }
+
+    const [createdAgent] = await db
+      .insert(agents)
+      .values({
+        ...input,
+        userId: ctx.auth.user.id, // ✅ fixed path
+      })
+      .returning();
+
+    return createdAgent;
+  })
+
 })
