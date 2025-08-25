18ba18a71198f6f8703e3c595118a6fd8ff02adc
sambhav-h <sambhavjain25102003@gmail.com>
2025-08-25 16:15:22 +0530

    feat: enhance agent detail view with loading and error components for improved user experience

--- src/app/(dashboard)/agents/[agentId]/page.tsx
+++ src/app/(dashboard)/agents/[agentId]/page.tsx
@@ -1,4 +1,4 @@
-import { AgentIdView } from "@/modules/agents/ui/views/agent-id-view";
+import { AgentIdView, AgentsIdViewErrror, AgentsIdViewLoading } from "@/modules/agents/ui/views/agent-id-view";
 import { getQueryClient,trpc } from "@/trpc/server"
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { Suspense } from "react";
@@ -16,8 +16,8 @@ const Page = async ({params}:Props) => {
     )
     return (
         <HydrationBoundary state={dehydrate(queryClient)}>
-            <Suspense fallback={<p>Loading</p>}>
-                <ErrorBoundary fallback={<p>Error</p>}>
+            <Suspense fallback={<p><AgentsIdViewLoading/></p>}>
+                <ErrorBoundary fallback={<p><AgentsIdViewErrror/></p>}>
                 <AgentIdView agentId={agentId}/>
                 </ErrorBoundary>
             </Suspense>
