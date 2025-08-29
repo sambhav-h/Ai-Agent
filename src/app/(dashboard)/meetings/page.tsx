68b3b846478b31bdb596826fbb280dfadb2a2400
sambhav-h <sambhavjain25102003@gmail.com>
2025-08-29 17:59:32 +0530

    feat: implement meetings management features including create, update, and view functionalities, enhance UI with new components and dialogs

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -1,23 +1,39 @@
-import { MeetingsView, MeetingsViewErrror, MeetingsViewLoading } from "@/modules/meetings/ui/views/meetings-view"
-import { getQueryClient, trpc } from "@/trpc/server"
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
-import { Suspense } from "react"
-import { ErrorBoundary } from "react-error-boundary"
+import { auth } from "@/lib/auth";
+import { MeetingsListHeader } from "@/modules/meetings/ui/components/meetings-list-header";
+import {
+  MeetingsView,
+  MeetingsViewErrror,
+  MeetingsViewLoading,
+} from "@/modules/meetings/ui/views/meetings-view";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
-const Page = () => {
-    const queryClient = getQueryClient()
-    void queryClient.prefetchQuery(
-        trpc.meetings.getMany.queryOptions({})
-    )
-    return (
-        <HydrationBoundary state = {dehydrate(queryClient)}>
-            <Suspense fallback={<MeetingsViewLoading/>}>
-                <ErrorBoundary fallback={<MeetingsViewErrror/>}>
-                    <MeetingsView/>
-                </ErrorBoundary>
-            </Suspense>
-        </HydrationBoundary>
-    )
-}
+const Page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
-export default Page
+  if (!session) {
+    redirect("/sign-in");
+  }
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.meetings.getMany.queryOptions({}));
+  return (
+    <>
+      <MeetingsListHeader />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<MeetingsViewLoading />}>
+          <ErrorBoundary fallback={<MeetingsViewErrror />}>
+            <MeetingsView />
+          </ErrorBoundary>
+        </Suspense>
+      </HydrationBoundary>
+    </>
+  );
+};
+
+export default Page;
--- src/modules/meetings/server/procedure.ts
+++ src/modules/meetings/server/procedure.ts
@@ -11,8 +11,47 @@ import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
 import { and, count, desc, eq, getTableColumns, ilike} from "drizzle-orm";
 import { z } from "zod";
+import { meetingsInsertSchema, meetingsUpdateSchema } from "../schemas";
 
 export const meetingsRouter = createTRPCRouter({
+      update: protectedProcedure
+        .input(meetingsUpdateSchema)
+        .mutation(async ({ ctx, input }) => {
+          const [updatedMeeting] = await db
+            .update(meetings)
+            .set(input)
+            .where(
+              and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id))
+            )
+            .returning();
+    
+          if (!updatedMeeting) {
+            throw new TRPCError({
+              code: "NOT_FOUND",
+              message: "Meeting not found",
+            });
+          }
+          updatedMeeting;
+        }),
+    
+    create: protectedProcedure
+        .input(meetingsInsertSchema)
+        .mutation(async ({ input, ctx }) => {
+          if (!ctx.auth?.user.id) {
+            throw new TRPCError({ code: "UNAUTHORIZED" });
+          }
+    
+          const [createdMeeting] = await db
+            .insert(meetings)
+            .values({
+              ...input,
+              userId: ctx.auth.user.id, // ✅ fixed path
+            })
+            .returning();
+    
+          return createdMeeting;
+        }),
+
   getOne: protectedProcedure
     .input(z.object({ id: z.string() }))
     .query(async ({ input, ctx }) => {
