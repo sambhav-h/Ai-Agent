b61e3dfe4b82942a994f36e1496730a4e24d2469
sambhav-h <sambhavjain25102003@gmail.com>
2025-08-31 18:03:42 +0530

    feat: implement meeting details view with loading and error states, add confirmation dialog for meeting removal, and enhance meeting editing functionality

--- src/app/(dashboard)/meetings/[meetingId]/page.tsx
+++ src/app/(dashboard)/meetings/[meetingId]/page.tsx
@@ -1,9 +1,44 @@
-const Page = () => {
-    return ( 
-        <div>
-            Meeting ID page
-        </div>
-     );
+import { auth } from "@/lib/auth";
+import { MeetingIdView, MeetingIdViewErrror, MeetingIdViewLoading } from "@/modules/meetings/ui/components/meeting-id-view";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+interface Props {
+  params: Promise<{
+    meetingId: string;
+  }>;
 }
- 
+
+const Page = async ({ params }: Props) => {
+  const { meetingId } = await params;
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/sign-in");
+  }
+
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.meetings.getOne.queryOptions({ id: meetingId })
+  );
+  return (
+    <div>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<MeetingIdViewLoading/>}>
+        <ErrorBoundary fallback={<MeetingIdViewErrror/>}>
+            <MeetingIdView meetingId={meetingId} />
+          </ErrorBoundary>
+        </Suspense>
+      </HydrationBoundary>
+    </div>
+  );
+};
+
 export default Page;
--- src/modules/meetings/server/procedure.ts
+++ src/modules/meetings/server/procedure.ts
@@ -15,6 +15,26 @@ import { meetingsInsertSchema, meetingsUpdateSchema } from "../schemas";
 import { MeetingStatus } from "../types";
 
 export const meetingsRouter = createTRPCRouter({
+    remove: protectedProcedure
+    .input(z.object({id:z.string()}))
+    .mutation(async ({ ctx, input }) => {
+      const [removedMeeting] = await db
+        .update(meetings)
+        .set(input)
+        .where(
+          and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id))
+        )
+        .returning();
+
+      if (!removedMeeting) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Meeting not found",
+        });
+      }
+      removedMeeting;
+    }),
+
   update: protectedProcedure
     .input(meetingsUpdateSchema)
     .mutation(async ({ ctx, input }) => {
@@ -59,8 +79,13 @@ export const meetingsRouter = createTRPCRouter({
       const [existingMeeting] = await db
         .select({
           ...getTableColumns(meetings),
+          agent: agents,
+          duration: sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))`.as(
+            "duration"
+          ),
         })
         .from(meetings)
+        .innerJoin(agents, eq(meetings.agentId,agents.id))
         .where(
           and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id))
         );
